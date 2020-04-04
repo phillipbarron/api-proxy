95391d402e0753a7f6e7ce9a7831a0434eb313b2
Phill Barron <phillipbarron242@gmail.com>
2020-04-04 17:52:14 +0100

    basic proxying working

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const getProxyMiddleware = require('./proxy-middleware')
 const app = express()
 const port = 3000
 
+app.use('/api', getProxyMiddleware.getProxyMiddleware());
 app.get('/', (req, res) => res.send('WHAT?'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
